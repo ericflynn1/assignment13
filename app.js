faf07a9a5027a6368e7322d932742b506dee0d5d
Eric <ericgflynn@gmail.com>
2016-08-23 07:47:21 -0400

    fixes

--- app.js
+++ app.js
@@ -1,67 +1,34 @@
-function getGames () {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'http://charlotte-games-rater.herokuapp.com/games');
-    request.addEventListener('load', function() {
-        let games = JSON.parse(request.responseText);
-        console.log('games');
-        let parent = document.querySelector('#game-list');
 
-        for (let i = 0; i <games.length; i++) {
-            let element = document.createElement('li');
-            element.textContent = games[i].name + '(' + games[i].rating + ' stars';
-            parent.appendChild(element);
-        }
-    });
-    request.send();
-}
-function addNewGame () {
-    let game = {
-        name: document.querySelector('#new-name').value,
-        rating: parseInt(document.querySelector('#new-rating').value),
-        releaseDate: parseInt(document.querySelector('#release-date').value),
-    };
-        let request = new XMLHttpRequest();
-        request.open('POST', 'http://charlotte-games-rater.herokuapp.com/games');
-        request.send(JSON.stringify(game));
-}
-window.addEventListener('load', function () {
-    console.log('testing');
-    getGames();
-    let submitBtn = document.querySelector('button');
-    submitBtn.addEventListener('click', function () {
-        addNewGame();
-    });
-});
 console.log('-------------------------------------------------------');
 function getCawfee () {
     let request = new XMLHttpRequest();
     request.open('GET', 'http://charlotte-coffee-shops.herokuapp.com/coffee-shops');
     request.addEventListener('load', function() {
-        let games = JSON.parse(request.responseText);
-        console.log('games');
-        let parent = document.querySelector('#game-list');
+        let coffee = JSON.parse(request.responseText);
+        console.log('coffee');
+        let parent = document.querySelector('#coffee-list');
 
-        for (let i = 0; i <games.length; i++) {
+        for (let i = 0; i < coffee.length; i++) {
             let element = document.createElement('li');
-            element.textContent = games[i].name + '(' + games[i].rating + ' stars';
+            element.textContent = [i]coffee.name + '(' + coffee[i].rating + ' stars';
             parent.appendChild(element);
         }
     });
     request.send();
 }
 function addNewCawfee () {
-    let game = {
-        name: document.querySelector('#new-name').value,
-        rating: parseInt(document.querySelector('#new-rating').value),
-        releaseDate: parseInt(document.querySelector('#release-date').value),
+    let cafe = {
+        name: document.querySelector('#cafe-name').value,
+        rating: parseInt(document.querySelector('#cafe-rating').value),
+        price: parseInt(document.querySelector('#price').value),
     };
         let request = new XMLHttpRequest();
         request.open('POST', 'http://charlotte-coffee-shops.herokuapp.com/coffee-shops');
-        request.send(JSON.stringify(game));
+        request.send(JSON.stringify(cawfee));
 }
 window.addEventListener('load', function () {
     console.log('testing');
-    getGames();
+    getCawfee();
     let submitBtn = document.querySelector('button');
     submitBtn.addEventListener('click', function () {
         addNewCawfee();
@@ -72,33 +39,33 @@ window.addEventListener('load', function () {
 console.log('-------------------------------------------------------');
 function getCars () {
     let request = new XMLHttpRequest();
-    request.open('GET', 'http://charlotte-coffee-shops.herokuapp.com/coffee-shops');
+    request.open('GET', 'https://stormy-retreat-24546.herokuapp.com/cars');
     request.addEventListener('load', function() {
-        let games = JSON.parse(request.responseText);
-        console.log('games');
-        let parent = document.querySelector('#game-list');
+        let cars = JSON.parse(request.responseText);
+        console.log('carss');
+        let parent = document.querySelector('#car-list');
 
-        for (let i = 0; i <games.length; i++) {
+        for (let i = 0; i < cars.length; i++) {
             let element = document.createElement('li');
-            element.textContent = games[i].name + '(' + games[i].rating + ' stars';
+            element.textContent = cars[i].name + '(' + cars[i].rating + ' stars';
             parent.appendChild(element);
         }
     });
     request.send();
 }
-function addNewCawfee () {
-    let game = {
-        name: document.querySelector('#new-name').value,
-        rating: parseInt(document.querySelector('#new-rating').value),
-        releaseDate: parseInt(document.querySelector('#release-date').value),
+function addNewCar () {
+    let carzo = {
+        model: document.querySelector('#new-model').value,
+        color: parseInt(document.querySelector('#color').value),
+        year: parseInt(document.querySelector('#year').value),
     };
         let request = new XMLHttpRequest();
-        request.open('POST', 'http://charlotte-coffee-shops.herokuapp.com/coffee-shops');
-        request.send(JSON.stringify(game));
+        request.open('POST', 'https://stormy-retreat-24546.herokuapp.com/cars');
+        request.send(JSON.stringify(cars));
 }
 window.addEventListener('load', function () {
     console.log('testing');
-    getGames();
+    getCars();
     let submitBtn = document.querySelector('button');
     submitBtn.addEventListener('click', function () {
         addNewCawfee();
